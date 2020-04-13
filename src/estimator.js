d5aef70c242b807de5d759b69ae4654b7d188425
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2020-04-13 18:07:50 +0100

    lint with eslint

--- src/estimator.js
+++ src/estimator.js
@@ -1,34 +1,58 @@
 const covid19ImpactEstimator = (data) => {
-    const {region, periodType, timeToElapse, reportedCases, population, totalHospitalBeds} = data;
-    const impact = {};
-    const severeImpact = {};
-    impact.currentlyInfected = reportedCases * 10;
-    severeImpact.currentlyInfected = reportedCases * 50;
-    if(periodType === "weeks") timeToElapse = timeToElapse * 7;
-    if(periodType === "months") timeToElapse = timeToElapse * 30;
-    impact.infectionsByRequestedTime = currentlyInfected * (2**((1/3)*timeToElapse))
-    severeImpact.infectionsByRequestedTime = currentlyInfected * (2**((1/3)*timeToElapse))
+  const {
+    region,
+    periodType,
+    timeToElapse,
+    reportedCases,
+    totalHospitalBeds
+  } = data;
 
-    impact.severeCasesByRequestedTime = (15/100) * impact.infectionsByRequestedTime
-    severeImpact.severeCasesByRequestedTime = (15/100) * severImpact.infectionsByRequestedTime
+  const impact = {};
+  const severeImpact = {};
+  impact.currentlyInfected = reportedCases * 10;
+  severeImpact.currentlyInfected = reportedCases * 50;
+  let computedTimeToElapse = timeToElapse;
+  if (periodType === 'weeks') computedTimeToElapse = timeToElapse * 7;
+  if (periodType === 'months') computedTimeToElapse = timeToElapse * 30;
 
-    impact.hospitalBedsByRequestedTime = ((35/100) * totalHospitalBeds) - impact.severeCasesByRequestedTime
-    severeImpact.hospitalBedsByRequestedTime = ((35/100) * totalHospitalBeds) - severeImpact.severeCasesByRequestedTime
+  impact.infectionsByRequestedTime = (
+    impact.currentlyInfected * (2 ** ((1 / 3) * computedTimeToElapse))
+  );
+  severeImpact.infectionsByRequestedTime = (
+    severeImpact.currentlyInfected * (2 ** ((1 / 3) * computedTimeToElapse))
+  );
 
-    impact.casesForICUByRequestedTime = (5/100) * impact.infectionsByRequestedTime
-    severeImpact.casesForICUByRequestedTime = (5/100) * severeImpact.infectionsByRequestedTime
+  impact.severeCasesByRequestedTime = (15 / 100) * impact.infectionsByRequestedTime;
+  severeImpact.severeCasesByRequestedTime = (15 / 100) * severeImpact.infectionsByRequestedTime;
 
-    impact.casesForVentilatorsByRequestedTime = (2/100) * impact.infectionsByRequestedTime
-    severeImpact.casesForVentilatorsByRequestedTime = (2/100) * severeImpact.infectionsByRequestedTime
+  impact.hospitalBedsByRequestedTime = (
+    ((35 / 100) * totalHospitalBeds) - impact.severeCasesByRequestedTime
+  );
+  severeImpact.hospitalBedsByRequestedTime = (
+    ((35 / 100) * totalHospitalBeds) - severeImpact.severeCasesByRequestedTime
+  );
 
-    impact.dollarsInFlight = impact.infectionsByRequestedTime * region.avgDailyIncomeInUSD * region.avgDailyIncomePopulation * timeToElapse
-    severeImpact.dollarsInFlight = severeImpact.infectionsByRequestedTime * region.avgDailyIncomeInUSD * region.avgDailyIncomePopulation * timeToElapse
+  impact.casesForICUByRequestedTime = (5 / 100) * impact.infectionsByRequestedTime;
+  severeImpact.casesForICUByRequestedTime = (5 / 100) * severeImpact.infectionsByRequestedTime;
 
-    return {
-        data,
-        impact,
-        severeImpact
-    }
+  impact.casesForVentilatorsByRequestedTime = (2 / 100) * impact.infectionsByRequestedTime;
+  severeImpact.casesForVentilatorsByRequestedTime = (
+    (2 / 100) * severeImpact.infectionsByRequestedTime
+  );
+
+  impact.dollarsInFlight = (
+    impact.infectionsByRequestedTime * region.avgDailyIncomeInUSD
+     * region.avgDailyIncomePopulation * computedTimeToElapse
+  );
+  severeImpact.dollarsInFlight = (
+    severeImpact.infectionsByRequestedTime * region.avgDailyIncomeInUSD
+     * region.avgDailyIncomePopulation * computedTimeToElapse
+  );
+  return {
+    data,
+    impact,
+    severeImpact
+  };
 };
 
 export default covid19ImpactEstimator;
