beeef179d8216f2808d7364e4be12e74a64149f3
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2020-04-13 17:32:39 +0100

    complete challenge 1 to 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,34 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const {region, periodType, timeToElapse, reportedCases, population, totalHospitalBeds} = data;
+    const impact = {};
+    const severeImpact = {};
+    impact.currentlyInfected = reportedCases * 10;
+    severeImpact.currentlyInfected = reportedCases * 50;
+    if(periodType === "weeks") timeToElapse = timeToElapse * 7;
+    if(periodType === "months") timeToElapse = timeToElapse * 30;
+    impact.infectionsByRequestedTime = currentlyInfected * (2**((1/3)*timeToElapse))
+    severeImpact.infectionsByRequestedTime = currentlyInfected * (2**((1/3)*timeToElapse))
+
+    impact.severeCasesByRequestedTime = (15/100) * impact.infectionsByRequestedTime
+    severeImpact.severeCasesByRequestedTime = (15/100) * severImpact.infectionsByRequestedTime
+
+    impact.hospitalBedsByRequestedTime = ((35/100) * totalHospitalBeds) - impact.severeCasesByRequestedTime
+    severeImpact.hospitalBedsByRequestedTime = ((35/100) * totalHospitalBeds) - severeImpact.severeCasesByRequestedTime
+
+    impact.casesForICUByRequestedTime = (5/100) * impact.infectionsByRequestedTime
+    severeImpact.casesForICUByRequestedTime = (5/100) * severeImpact.infectionsByRequestedTime
+
+    impact.casesForVentilatorsByRequestedTime = (2/100) * impact.infectionsByRequestedTime
+    severeImpact.casesForVentilatorsByRequestedTime = (2/100) * severeImpact.infectionsByRequestedTime
+
+    impact.dollarsInFlight = impact.infectionsByRequestedTime * region.avgDailyIncomeInUSD * region.avgDailyIncomePopulation * timeToElapse
+    severeImpact.dollarsInFlight = severeImpact.infectionsByRequestedTime * region.avgDailyIncomeInUSD * region.avgDailyIncomePopulation * timeToElapse
+
+    return {
+        data,
+        impact,
+        severeImpact
+    }
+};
 
 export default covid19ImpactEstimator;
